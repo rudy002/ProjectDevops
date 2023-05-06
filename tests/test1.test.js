0edbbfb09df50530c87e60f01afb1bc555c4a319
rudy002 <rudyhaddad.job@gmail.com>
2023-05-06 22:26:30 +0300

    first commit

---
+++ tests/test1.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../server');
+
+describe("Test suit 1:", () => {
+    test("test 1: ", async ()=>{
+        const res = await request(app).get('/');
+        expect(res.statusCode).toEqual(200); //work properly
+    })
+    test("test 2: ", async()=>{
+        const res = await request(app).get('/1234');
+        expect(res.statusCode).toEqual(404);//404 not found
+    })
+});
