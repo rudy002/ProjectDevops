f289069fb1e6497d9110f45168c2577f77f751e0
rudy002 <rudyhaddad.job@gmail.com>
2023-05-15 11:03:46 +0300

    ajout code html

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require('express');
-//for interacting with the frontend
+const app = express();
 const path = require('path');
 
-const app = express();
+// Vos autres routes et middleware ici
 
-app.get('/api',  (req, res) => {
-    res.send('Hello World from the API');
-    });
-app.get('/api/test', (req, res) => {
-    res.send('Test Hello');
-    });
+app.use(express.static(path.join(__dirname, 'public')));
 
-    app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-    app.get('/index', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    });
+// Le reste de votre code
 
+const port = process.env.PORT || 3000;
 
-
-module.exports = app;
+app.listen(port, () => {
+    console.log('Server Started');
+});
